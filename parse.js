d1fc04227dd67fa3a88b5622e62e74f4ec68e49b
MarcelKaemper <marcelkaemper862@gmail.com>
2019-12-25 00:38:14 +0100

    Fixed parsing of .txt file

--- parse.js
+++ parse.js
@@ -8,9 +8,16 @@ const parse = (evt) => {
         // console.log(reader.result);
         let res = reader.result;
         res = res.replace(/[\r\n]+/gm, "");
-        for(let x = 0; x<res.length; x++){
-            console.log(res.charAt(x));
-            let ch = res.charAt(x);
+        // for(let x = 0; x<res.length; x++){
+        //     console.log(res.charAt(x));
+            // let ch = res.charAt(x);
+        for(let i = 0; i<9; i++){ 
+            let row = document.getElementsByClassName("row"+i)[0];
+            for(let j = 0; j<9; j++){ 
+                console.log(j);
+                row.childNodes.item(j).innerHTML = res.charAt((i*9)+j);
+            }
         }
+        // }
     }
 }
