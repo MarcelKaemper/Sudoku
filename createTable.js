628344f9a90e2983c9074700a17789c67fdaed0f
MarcelKaemper <marcelkaemper862@gmail.com>
2020-01-05 10:32:34 +0100

    Adding square class to gametable

--- createTable.js
+++ createTable.js
@@ -16,3 +16,32 @@ for(let i = 0; i<9; i++){
     tbl.appendChild(tr);
 }
 body.appendChild(tbl);
+
+// for(let i = 0; i<3; i++) {
+//     let rows = tbl.children;
+//     for(let j = 0; j<3; j++) {
+//         for(let k = 0; k<3; k++) {
+//             let activeRow = rows[k+(j*3)];
+//             for(let l = 0; l<3; l++) {
+//                 //console.log(activeRow.children[l].children[0]);
+//                 activeRow.children[l+(k*3)].classList.add("square_"+l);
+//             }
+//         }
+//     }
+// }
+
+let rows = tbl.children;
+let cols = rows[0].children;
+
+for(let i = 0; i<3; i++) {
+    for(let j = 0; j<3; j++) {
+        let cols = rows[j+(i*3)].children;
+        for(let k = 0; k<3; k++) {
+            for(let l = 0; l<3; l++) {
+                cols[l+(k*3)].classList.add("Square_"+(0+k+(3*i)));
+                // console.log(k*l);
+                // console.log(cols[l+(k*3)]);
+            }
+        }
+    }
+}
